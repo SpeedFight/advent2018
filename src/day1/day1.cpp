f4160b2b87d4c97e1c8e8a82d3ee897d8bc54c9d
SF <speedfight_2@wp.pl>
2018-12-06 19:28:19 +0100

    day 1 complete

--- src/day1/day1.cpp
+++ src/day1/day1.cpp
@@ -6,10 +6,64 @@
  */
 
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <string>
+#include <set>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+void static day1p1(){
+	char data[10];
+	int sum = 0;
+
+	std::fstream file;
+	file.open("src/day1/input.txt", std::ios::in);
+	while(file.getline(data, 10)){
+		std::string text(data);
+		sum += std::stoi(text);
+	}
+
+	file.close();
+
+	std::cout<< sum;
 }
+
+void static day1p2(){
+	char data[10];
+	int sum = 0;
+	bool repeat = false;
+
+	std::set<int> numbers;
+
+	std::fstream file;
+	file.open("src/day1/input.txt", std::ios::in);
+	while(!repeat){
+		while(file.getline(data, 10)){
+			std::string text(data);
+			sum += std::stoi(text);
+
+			if(numbers.find(sum) == numbers.end()){
+				numbers.insert(sum);
+			} else {
+				repeat = true;
+				break;
+			}
+
+		}
+		if(repeat){
+			break;
+		}
+
+		file.clear();
+		file.seekg(0, std::ios::beg);
+
+	}
+	file.close();
+
+
+	std::cout << sum;
+}
+
+//int main() {
+//
+//	return 0;
+//}
 
