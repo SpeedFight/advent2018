ef5e3d7eeec4859c154832398394d33e5924162e
SF <speedfight_2@wp.pl>
2018-12-06 18:33:05 +0100

    fist

---
+++ src/day1/day1.cpp
@@ -0,0 +1,15 @@
+/*
+ * day1.cpp
+ *
+ *  Created on: 6 Dec 2018
+ *      Author: speedfight
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
