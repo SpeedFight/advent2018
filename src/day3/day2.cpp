2482cedc1556d98785d3bbcf900a5d9e2149f726
SF <speedfight_2@wp.pl>
2018-12-10 19:44:56 +0100

    add files for day 3 and template

---
+++ src/day3/day2.cpp
@@ -0,0 +1,52 @@
+/*
+ * day1.cpp
+ *
+ *  Created on: 6 Dec 2018
+ *      Author: speedfight
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+#include <string.h>
+
+
+void static day3p1(){
+	char data[30];
+
+	std::fstream file;
+	file.open("src/day3/input.txt", std::ios::in);
+	while(file.getline(data, 30)){
+	}
+
+	file.close();
+
+	std::cout << "Answer is: " << "" << std::endl;
+}
+
+
+
+void static day3p2(){
+	char data[30];
+
+	std::fstream file;
+	file.open("src/day3/input.txt", std::ios::in);
+	while(file.getline(data, 30)){
+	}
+
+	file.close();
+
+	std::cout << "Answer is: " << "" << std::endl;
+}
+
+
+//int main() {
+//
+//	day2p1();
+//	day2p2();
+//
+//	return 0;
+//}
+
